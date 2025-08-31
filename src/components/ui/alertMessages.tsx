ef2f7983dd0736a96441a4c9b562316ab40d4b3b
kttsh <prodeinbss@gmail.com>
2025-08-31 19:06:54 +0900

    AlertMessagesコンポーネントの型定義を明示化し、メッセージのリスト表示においてキーを使用するように修正しました。これにより、リストの一意性が向上し、可読性が改善されました。

--- src/components/ui/alertMessages.tsx
+++ src/components/ui/alertMessages.tsx
@@ -5,15 +5,15 @@ type AlertMessageProps = {
 	messages: Record<string, string>;
 };
 
-export const AlertMessages: React.FC<AlertMessageProps> = ({ messages }) => {
+export const AlertMessages = ({ messages }: AlertMessageProps) => {
 	return (
 		<Alert variant="destructive">
 			<AlertCircleIcon />
 			<AlertTitle />
 			<AlertDescription>
 				<ul className="list-disc pl-4 space-y-1">
-					{Object.entries(messages).map(([_, value], index) => (
-						<li key={index}>{value}</li>
+					{Object.entries(messages).map(([key, value]) => (
+						<li key={key}>{value}</li>
 					))}
 				</ul>
 			</AlertDescription>
